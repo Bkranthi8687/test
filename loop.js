aa4eb246e60cc90fa9c4b1385ac4ed20dbf43d7a
unknown <bellamkranthibellam8687@gmail.com>
2025-01-20 18:03:42 +0530

    custom msgs

---
+++ loop.js
@@ -0,0 +1,50 @@
+// // //let i=1;
+// // //while(i<=5){
+// //   //  console.log("Good morning!!!");
+// //     //i++;
+// // //}
+// // //let users=["kranthi","shravan","Roger"]
+// // //let x=users.map((user)=>{
+// //  //   return user;
+// // //});
+// // //console.log(x);
+// // //let users=["kranthi","shravan","Roger"]
+// // //let x = users.forEach((user)=>{
+// //   //  return(user);
+
+// // //});
+// // //console.log(x);
+// // //var a=10;
+// // //var b=30;
+// // //function x(){
+// //   //var user
+
+// // //}
+
+// // let userDetails={
+// //   name:"Ankith",
+// //   age:25,
+// //   city:"Hyderabad"
+// // }
+// // console.log(userDetails);
+// // console.log(userDetails.name);
+// // console.log(typeof(userDetails));
+
+// // let x= JSON.stringify(userDetails);
+// // console.log(x);
+// // console.log(x.name);
+// // console.log(typeof(x));
+
+// // let y=JSON.parse(x);
+// // console.log(x);
+
+// localStorage.setItem("user1","raghu");
+// localStorage.setItem("user2","vamshi");
+// localStorage.setItem("user3","rahul");
+// localStorage.setItem("user4","shravan");
+// localStorage.setItem("user5","raju");
+// localStorage.setItem("user6","ramu");
+// let user6=localStorage.getitem("user6");
+// console.log(user6);
+// localStoarge.removeItem("user1");
+
