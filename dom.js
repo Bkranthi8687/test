b0db89a961900bb3284ed897e492fe831f728421
unknown <bellamkranthibellam8688@gmail.com>
2025-01-24 10:30:37 +0530

    new files pushed

---
+++ dom.js
@@ -0,0 +1,28 @@
+// // let element = document.getElementById("demo");
+// // element.innerText = "Dom";
+// // console.log(element);
+// // let div = document.getElementById("test");
+// // div.innerHtml="<h1>Header</h1>"
+// // console.log(div);
+
+
+// let ele=document.getElementsByClassName("test");
+// ele[0].computedStyleMap.backgroundColor="yellow"
+// let x=[...ele];
+// console.log(x,Array.isArray(x));
+// x.map((element)=>{
+//     element.style.backgroundColor="teal";
+
+
+// })
+// let ele = document .getElementsByName("username");
+// let ele = document.querySelector(".test");
+// console.log(ele);
+// [...ele].map(Element=>{
+//     console.log(Element.innerText);
+// })
+let btn=document.getElementById("btn")
+btn.addEventListener("click",()=>{
+    console.log("button clicked");
+    document.body.style
+})
